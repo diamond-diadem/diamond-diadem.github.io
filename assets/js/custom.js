5aa3c6733b5d59e6cc879e1ff62afc11354e6beb
David Martin-Calle <152533086+diamond-diadem@users.noreply.github.com>
2024-02-08 19:31:54 +0100

    Modifié custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,25 +1,45 @@
 // Put your custom JS code here
 
-const topButton = document.getElementById('toTop');
+// const topButton = document.getElementById('toTop');
 
-// if (topButton !== null) {
-  topButton.classList.remove('fade');
-  window.onscroll = function() {
-    scrollFunction()
-  };
+// // if (topButton !== null) {
+//   topButton.classList.remove('fade');
+//   window.onscroll = function() {
+//     scrollFunction()
+//   };
 
-  topButton.addEventListener('click', topFunction);
+//   topButton.addEventListener('click', topFunction);
+// // }
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 270 || document.documentElement.scrollTop > 270) {
+//     topButton.classList.add('fade');
+//   } else {
+//     topButton.classList.remove('fade');
+//   }
 // }
+
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+// Get the button:
+let mybutton = document.getElementById("toTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 270 || document.documentElement.scrollTop > 270) {
-    topButton.classList.add('fade');
+    mybutton.style.display = "block";
   } else {
-    topButton.classList.remove('fade');
+    mybutton.style.display = "none";
   }
 }
 
+// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
