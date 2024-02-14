22588d0eda0c5aae07bc6095a7cae03cac0e03c5
EXT David MARTIN-CALLE <david.martin-calle@univ-lyon1.fr>
2024-02-14 15:16:10 +0100

    encore un test déploiement toTopButton

--- assets/js/to-top.js
+++ assets/js/to-top.js
@@ -10,7 +10,7 @@ if (topButton !== null) {
 }
 
 function scrollFunction() {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+  if (document.body.scrollTop > 270 || document.documentElement.scrollTop > 270) {
     topButton.classList.add('fade');
   } else {
     topButton.classList.remove('fade');
@@ -21,3 +21,28 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+// $(document).ready(function() {
+//     $(window).scroll(function() {
+//         if($(this).scrollTop() > 100){
+//             $('#toTop').stop().animate({
+//                 top: '20px'    
+//                 }, 500);
+//         }
+//         else{
+//             $('#toTop').stop().animate({
+//                top: '-100px'    
+//             }, 500);
+//         }
+//     });
+//     $('#toTop').click(function() {
+//         $('html, body').stop().animate({
+//            scrollTop: 0
+//         }, 500, function() {
+//            $('#toTop').stop().animate({
+//                top: '-100px'    
+//            }, 500);
+//         });
+//     });
+// });   
