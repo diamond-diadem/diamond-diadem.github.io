122c07232d7241224a49c60c89b65e396e67d84d
David MARTIN-CALLE <david.martin-calle@univ-lyon1.fr>
2024-05-31 11:43:20 +0200

    another attempt

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 // Put your custom JS code here
 
-const topButton = document.getElementById('toTop');
+let topButton = document.getElementById('toTop');
 
 if (topButton !== null) {
   topButton.classList.remove('fade');
@@ -23,3 +23,24 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+// // Get the button:
+// let mybutton = document.getElementById("toTop");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// } 
