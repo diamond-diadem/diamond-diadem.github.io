9d09d75233b637939fc3bfe903f6ee7dc78608b5
David MARTIN-CALLE <david.martin-calle@univ-lyon1.fr>
2024-12-20 21:28:06 +0100

    Start impplementing solution to all videos

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const placeholders = document.querySelectorAll('.youtube-placeholder');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const placeholders = document.querySelectorAll('.youtube-placeholder');
 
-  placeholders.forEach(placeholder => {
-      const button = placeholder.querySelector('.popup-button');
+//   placeholders.forEach(placeholder => {
+//       const button = placeholder.querySelector('.popup-button');
 
-      // Ajuster la taille de la police en fonction de la largeur du conteneur
-      const adjustFontSize = () => {
-          const containerWidth = placeholder.offsetWidth; // Largeur réelle du conteneur
-          const fontSize = containerWidth * 0.02; // Taille de la police : 2% de la largeur
-          button.style.fontSize = `${fontSize}px`;
-      };
+//       // Ajuster la taille de la police en fonction de la largeur du conteneur
+//       const adjustFontSize = () => {
+//           const containerWidth = placeholder.offsetWidth; // Largeur réelle du conteneur
+//           const fontSize = containerWidth * 0.02; // Taille de la police : 2% de la largeur
+//           button.style.fontSize = `${fontSize}px`;
+//       };
 
-      adjustFontSize(); // Ajuste initialement
-      window.addEventListener('resize', adjustFontSize); // Ajuste lors des redimensionnements
-  });
-});
+//       adjustFontSize(); // Ajuste initialement
+//       window.addEventListener('resize', adjustFontSize); // Ajuste lors des redimensionnements
+//   });
+// });
