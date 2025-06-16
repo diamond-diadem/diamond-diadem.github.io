9c74e1c2fd89bf9a5fe167ea585f99a8e08bf100
EXT David MARTIN-CALLE <david.martin-calle@univ-lyon1.fr>
2025-06-16 23:24:58 +0200

    Reorganize imports in app.js and clean up custom.js by removing commented-out code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const placeholders = document.querySelectorAll('.youtube-placeholder');
-
-//   placeholders.forEach(placeholder => {
-//       const button = placeholder.querySelector('.popup-button');
-
-//       // Ajuster la taille de la police en fonction de la largeur du conteneur
-//       const adjustFontSize = () => {
-//           const containerWidth = placeholder.offsetWidth; // Largeur réelle du conteneur
-//           const fontSize = containerWidth * 0.02; // Taille de la police : 2% de la largeur
-//           button.style.fontSize = `${fontSize}px`;
-//       };
-
-//       adjustFontSize(); // Ajuste initialement
-//       window.addEventListener('resize', adjustFontSize); // Ajuste lors des redimensionnements
-//   });
-// });
+// Place your custom JavaScript code here.
+// This file is loaded after all other scripts,
+// so you can use it to add custom functionality or override existing functions.
