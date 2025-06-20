e646dfb9f60de5861f0b6683ad5996820ac1d6dc
EXT David MARTIN-CALLE <david.martin-calle@univ-lyon1.fr>
2025-06-20 12:08:06 +0200

    Add automatic detection of apptainer pull commands and of container image name fo click counting with Google Analytics

--- assets/js/clipboard.js
+++ assets/js/clipboard.js
@@ -7,7 +7,10 @@ import Clipboard from 'clipboard';
 
     for (var i = 0; i < cb.length; ++i) {
         var element = cb[i];
-        element.insertAdjacentHTML('afterbegin', '<div class="copy"><button title="Copy to clipboard" class="btn-copy" aria-label="Clipboard button"><div></div></button></div>');
+        element.insertAdjacentHTML(
+            'afterbegin',
+            '<div class="copy"><button title="Copy to clipboard" class="btn-copy" aria-label="Clipboard button"><div></div></button></div>'
+        );
     }
 
     var clipboard = new Clipboard('.btn-copy', {
@@ -17,21 +20,17 @@ import Clipboard from 'clipboard';
     });
 
     clipboard.on('success', function (e) {
-        // Send click event to Google Analytics for apptainer-pull blocks only
-        // Look for 'apptainer-pull' and the next class (e.g. 'abinit')
         var codeBlock = e.trigger.closest('.highlight');
-        if (codeBlock && codeBlock.classList.contains('apptainer-pull')) {
-            // Find all classes
-            var clist = Array.from(codeBlock.classList);
-            var idx = clist.indexOf('apptainer-pull');
-            // The grouping class is the next one after 'apptainer-pull'
-            if (idx !== -1 && idx < clist.length - 1) {
-                var groupClass = clist[idx + 1];
-                // Send event to Google Analytics
+        if (codeBlock) {
+            var pre = codeBlock.querySelector('pre');
+            var firstLine = pre ? pre.textContent.trim().split(/\r?\n/)[0] : '';
+            var match = firstLine.match(/^apptainer pull ([a-z0-9_-]+)\.sif/);
+            if (match) {
+                var groupClass = match[1];
                 if (typeof gtag === 'function') {
                     gtag('event', 'copy_apptainer_pull', {
-                        'event_category': 'copy',
-                        'event_label': groupClass
+                        event_category: 'copy',
+                        event_label: groupClass
                     });
                 } else if (typeof ga === 'function') {
                     ga('send', 'event', 'copy', 'apptainer_pull', groupClass);
@@ -46,43 +45,3 @@ import Clipboard from 'clipboard';
         console.error('Trigger:', e.trigger);
     });
 })();
-
-// /*!
-//  * clipboard.js for Bootstrap based Thulite sites
-//  * Copyright 2021-2024 Thulite
-//  * Licensed under the MIT License
-//  */
-
-// import Clipboard from 'clipboard';
-
-// (() => {
-//     'use strict';
-
-//     var cb = document.getElementsByClassName('highlight');
-
-//     for (var i = 0; i < cb.length; ++i) {
-//         var element = cb[i];
-//         element.insertAdjacentHTML('afterbegin', '<div class="copy"><button title="Copy to clipboard" class="btn-copy" aria-label="Clipboard button"><div></div></button></div>');
-//     }
-
-//     var clipboard = new Clipboard('.btn-copy', {
-//         target: function (trigger) {
-//             return trigger.parentNode.nextElementSibling;
-//         }
-//     });
-
-//     clipboard.on('success', function (e) {
-//         /*
-//       console.info('Action:', e.action);
-//       console.info('Text:', e.text);
-//       console.info('Trigger:', e.trigger);
-//       */
-
-//         e.clearSelection();
-//     });
-
-//     clipboard.on('error', function (e) {
-//         console.error('Action:', e.action);
-//         console.error('Trigger:', e.trigger);
-//     });
-// })();
